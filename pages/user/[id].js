3da96c4fdb5b3875946e55c966d832f4e7ef9987
thatSchloesser <sschloesser2@gmail.com>
2021-02-04 12:34:54 -0600

    non-static dynamic route refactor

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,20 +1,7 @@
-export async function getStaticPaths() {
-  //dynamic paths with this config:
-  return {
-    paths: [],
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  //params comes from getStaticPaths automatically, id comes form the [id] filename
-  return {
-    props: {
-      path: params.id,
-    },
-  };
-}
+import { useRouter } from 'next/router';
 
 export default function Post({ path }) {
-  return <>{path}</>;
+  const router = useRouter();
+  const { id } = router.query;
+  return <>id: {id}</>;
 }
