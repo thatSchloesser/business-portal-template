1f0b1a6416ad93d0ebf27ea655897962794b5115
thatSchloesser <sschloesser2@gmail.com>
2021-02-05 15:24:05 -0600

    input edit toggle

--- components/settings/index.js
+++ components/settings/index.js
@@ -1,21 +1,17 @@
+import { useState } from 'react';
+
+import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-
 import Divider from '@material-ui/core/Divider';
-import InboxIcon from '@material-ui/icons/Inbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-
-import { positions } from '@material-ui/system';
-
-import IconButton from '@material-ui/core/IconButton';
 import TextField from '@material-ui/core/TextField';
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
 
 import EditIcon from '@material-ui/icons/Edit';
+import SaveIcon from '@material-ui/icons/Save';
 
 function ListItemLink(props) {
   return <ListItem button component='a' {...props} />;
@@ -25,25 +21,39 @@ function ListItemLink(props) {
 
 /*
 1. make input toggle
-  float left vs right
 2. load user
 3. UPDARTE user
-
 
 */
 
 const Settings = () => {
+  const [editEmail, setEditEmail] = useState(false);
+  const [editGiven, setEditGiven] = useState(false);
+  const [editSurname, setEditSurname] = useState(false);
+
+  const postEmail = () => {
+    console.log('custom message');
+    //api call here
+    setEditEmail(false);
+  };
+
+  const postGivenName = () => {
+    console.log('custom message 2');
+    //api call here
+    setEditGiven(false);
+  };
+
+  const postSurname = () => {
+    console.log('custom message 3');
+    //api call here
+    setEditSurname(false);
+  };
+
   return (
     <>
       {/* <Avatar alt='Remy Sharp' src='/static/images/avatar/1.jpg' /> */}
       {/* <Divider /> */}
-      <List component='nav' aria-label='main mailbox folders'>
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText primary='Drafts' />
-        </ListItem>
+      <List>
         <Grid container>
           <Grid item sm={3} xs={12}>
             <ListItem>
@@ -60,27 +70,35 @@ const Settings = () => {
                 placeholder='placeholder'
                 multiline
                 fullWidth
+                disabled={!editEmail}
               />
               <ListItemSecondaryAction>
-                <IconButton edge='end' aria-label='delete'>
-                  <EditIcon />
-                </IconButton>
+                {!editEmail ? (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => setEditEmail(true)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => postEmail()}
+                  >
+                    <SaveIcon />
+                  </IconButton>
+                )}
               </ListItemSecondaryAction>
             </ListItem>
           </Grid>
         </Grid>
         <Divider />
-        <ListItem button>
-          <ListItemIcon>
-            <DraftsIcon />
-          </ListItemIcon>
-          <ListItemText primary='Drafts' />
-        </ListItem>
-        <Divider />
         <Grid container>
           <Grid item sm={3} xs={12}>
             <ListItem>
-              <ListItemText>Email </ListItemText>
+              <ListItemText>Given Name </ListItemText>
             </ListItem>
           </Grid>
           <Grid item sm={9} xs={12}>
@@ -93,25 +111,71 @@ const Settings = () => {
                 placeholder='placeholder'
                 multiline
                 fullWidth
+                disabled={!editGiven}
               />
               <ListItemSecondaryAction>
-                <IconButton edge='end' aria-label='delete'>
-                  <EditIcon />
-                </IconButton>
+                {!editGiven ? (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => setEditGiven(true)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => postGivenName()}
+                  >
+                    <SaveIcon />
+                  </IconButton>
+                )}
               </ListItemSecondaryAction>
             </ListItem>
           </Grid>
         </Grid>
-      </List>
-      <Divider />
-
-      <List component='nav' aria-label='secondary mailbox folders'>
-        <ListItem button>
-          <ListItemText primary='Trash' />
-        </ListItem>
-        <ListItemLink href='#simple-list'>
-          <ListItemText primary='Spam' />
-        </ListItemLink>
+        <Divider />
+        <Grid container>
+          <Grid item sm={3} xs={12}>
+            <ListItem>
+              <ListItemText>Surname </ListItemText>
+            </ListItem>
+          </Grid>
+          <Grid item sm={9} xs={12}>
+            {/* kind of strange that a graph lets us stick this inline, but hey why not */}
+            {/* <ListItem pt={0} style={{ 'padding-top': '0' }}> */}
+            <ListItem>
+              <TextField
+                id='standard-textarea'
+                label='Placeholder'
+                placeholder='placeholder'
+                multiline
+                fullWidth
+                disabled={!editSurname}
+              />
+              <ListItemSecondaryAction>
+                {!editSurname ? (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => setEditSurname(true)}
+                  >
+                    <EditIcon />
+                  </IconButton>
+                ) : (
+                  <IconButton
+                    edge='end'
+                    aria-label='edit'
+                    onClick={() => postSurname()}
+                  >
+                    <SaveIcon />
+                  </IconButton>
+                )}
+              </ListItemSecondaryAction>
+            </ListItem>
+          </Grid>
+        </Grid>
       </List>
     </>
   );
