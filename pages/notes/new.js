13c3ba23973b655b142fa7d76c251de987f86c73
thatSchloesser <sschloesser2@gmail.com>
2021-03-02 15:52:22 -0600

    new note form configured

--- pages/notes/new.js
+++ pages/notes/new.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import Router from 'next/router';
+
 import Layout from '../../components/layout';
 
 import Grid from '@material-ui/core/Grid';
@@ -6,6 +9,8 @@ import useStyles from '../../styles/mui-styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 
 // import NotificationsIcon from '@material-ui/icons/Notifications';
 
@@ -25,44 +30,84 @@ import Paper from '@material-ui/core/Paper';
 //4) backend routes
 
 // markup
+
 const New = () => {
   const classes = useStyles();
 
-  const submitHandler = () => {
-    console.log('hello from submit handler');
-  };
+  const [submitting, setSubmitting] = useState(false);
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const res = await fetch('/api/notes/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          hi: 'hi',
+        }),
+      });
+      const json = await res.json();
+      setSubmitting(false);
+      if (!res.ok) throw Error(json.message);
+      Router.push('/notes');
+    } catch (e) {
+      throw Error(e.message);
+    }
+  }
+
+  const titleHandler = (e) => {};
 
   return (
     <Layout pageTitle={'New Note'}>
       <Grid container spacing={3}>
         <Grid item xs={12}>
           <Paper className={classes.paper}>
-            <form noValidate autoComplete='off' onSubmit={submitHandler}>
+            <form autoComplete='off' onSubmit={submitHandler}>
               <Grid container spacing={3}>
-                <Grid item xs={12}>
-                  <TextField id='standard-basic' label='Standard' />
-                </Grid>
-                <Grid item xs={12}>
+                <Grid item md={8} xs={12}>
                   <TextField
-                    id='filled-basic'
-                    label='Filled'
-                    variant='filled'
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <TextField
+                    required={true}
+                    fullWidth
                     multiline
-                    id='outlined-basic'
-                    label='Outlined'
+                    id='1'
+                    label='Title'
                     variant='outlined'
+                    inputProps={{ maxLength: 45 }}
+                    // onChange={titleHandler}
+                    // error={titleE}
+                    // helperText={titleHelper}
                   />
+                </Grid>
+                <Grid item xs={12}>
+                  {/* <TextareaAutosize /> */}
+
+                  <TextField
+                    fullWidth
+                    multiline
+                    id='2'
+                    label='Note'
+                    variant='outlined'
+                    required={true}
+                    rows={7}
+                    inputProps={{ maxLength: 500 }}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <div>
+                    <Button
+                      type='submit'
+                      variant='contained'
+                      color='primary'
+                      disabled={submitting}
+                    >
+                      {submitting ? 'Saving...' : 'Save'}
+                    </Button>
+                  </div>
                 </Grid>
               </Grid>
-              <div>
-                <Button type='submit' variant='contained' color='primary'>
-                  Save
-                </Button>
-              </div>
             </form>
           </Paper>
         </Grid>
